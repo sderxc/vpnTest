8a29a116dcd10b514c99f83e77a5dfb3bcf6acf3
sderxc <sderxc@gmail.com>
2015-07-18 22:03:59 +0300

    debug

--- service/weather.js
+++ service/weather.js
@@ -2,22 +2,23 @@ var http = require('http');
 
 module.exports = (function(){
     var API_URL = 'http://api.openweathermap.org/data/2.5/weather?id=519690&units=metric&lang=ru';
-    var apiResponce = (function(){
-        http.get(API_URL, function(res) {
-            console.log("Got response: " + res.statusCode);
-            var content = '';
-            res.on('data', function(chunk) {
-                content += chunk;
-            });
-            res.on('end', function() {
-                content =  JSON.parse(content);
-                return content;
-            });
-        }).on('error', function(e) {
-            console.log("Got error: " + e.message);
-            return {};
+    var apiResponce;
+
+    http.get(API_URL, function(res) {
+        var _self = this;
+        console.log(_self);
+        var content = '';
+        res.on('data', function(chunk) {
+            content += chunk;
         });
-    })();
+        res.on('end', function() {
+            _self.apiResponce =  JSON.parse(content);
+
+        });
+    }).on('error', function(e) {
+        console.log("Got error: " + e.message);
+    });
+
     return {
         getTemp: function() {
             return {
