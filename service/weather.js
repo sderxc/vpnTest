17908e8e74c25d44ec142d86268da979701d0f1a
sderxc <sderxc@gmail.com>
2015-07-18 21:25:42 +0300

    test weather module

--- service/weather.js
+++ service/weather.js
@@ -1,7 +1,8 @@
-var weather = function(){
-    this.test = function(){
-        console.log(123);
-    };
-};
 
-module.exports = weather;
+module.exports = (function(){
+    return {
+        test: function(){
+            console.log(123);
+        }
+    };
+})();
