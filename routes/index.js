253170a5ca54a2148c0c7fac1339c552baf75650
sderxc <sderxc@gmail.com>
2015-07-18 21:01:05 +0300

    test weather module

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var weather = require('./service/weather');
+var weather = require('../service/weather');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
