978aa3454a7bbbe73ea80f18cb8474feb5a21999
sderxc <sderxc@gmail.com>
2015-07-18 21:29:57 +0300

    add weather API

--- service/weather.js
+++ service/weather.js
@@ -1,8 +1,15 @@
+var http = require('http');
 
 module.exports = (function(){
+    var API_URL = 'http://api.openweathermap.org/data/2.5/weather?id=519690&units=metric&lang=ru';
     return {
         test: function(){
-            console.log(123);
+            http.get(API_URL, function(res) {
+                console.log(res);
+                console.log("Got response: " + res.statusCode);
+            }).on('error', function(e) {
+                console.log("Got error: " + e.message);
+            });
         }
     };
 })();
